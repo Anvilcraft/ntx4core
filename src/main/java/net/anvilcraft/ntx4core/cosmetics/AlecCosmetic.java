f498742076cce691f25eed73707d2e73221665b5
LordMZTE <lord@mzte.de>
2023-11-01 17:44:52 +0100

    fix: use Ntx4Core.id

--- src/main/java/net/anvilcraft/ntx4core/cosmetics/AlecCosmetic.java
+++ src/main/java/net/anvilcraft/ntx4core/cosmetics/AlecCosmetic.java
@@ -1,27 +1,28 @@
 package net.anvilcraft.ntx4core.cosmetics;
 
 import net.anvilcraft.anvillib.cosmetics.ICosmetic;
+import net.anvilcraft.ntx4core.Ntx4Core;
 import net.minecraft.util.Identifier;
 
 public class AlecCosmetic implements ICosmetic {
     @Override
     public Identifier getAnimationFileLocation() {
-        return new Identifier("ntx4core", "animations/alec.json");
+        return Ntx4Core.id("animations/alec.json");
     }
 
     @Override
     public Identifier getModelLocation() {
-        return new Identifier("ntx4core", "geo/alec.json");
+        return Ntx4Core.id("geo/alec.json");
     }
 
     @Override
     public Identifier getTextureLocation() {
-        return new Identifier("ntx4core", "textures/alec.png");
+        return Ntx4Core.id("textures/alec.png");
     }
 
     @Override
     public Identifier getID() {
-        return new Identifier("ntx4core", "alec");
+        return Ntx4Core.id("alec");
     }
 
     @Override
